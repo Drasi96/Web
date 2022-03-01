7149449bd4e70f98e9d63a2c49e1b701e35b8cd9
Drasi96 <zahorak.andras@gmail.com>
2022-03-01 14:12:25 +0000

    next method

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ adminRouter.get('/users', function(req, res) {
 adminRouter.get('/posts', function(req, res) {
  res.send('I show all the posts!'); });
 // apply the routes to our application
+///////////////////////////////////////////////////////////////////
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next(); });
+   ///////////////////////////////////////////////////////////////////
 app.use('https://sleepy-savannah-40413.herokuapp.com/admin', adminRouter);
 ///////////////////////////////////////////////////////////////////
  res.sendFile(__dirname + '/index.html');
