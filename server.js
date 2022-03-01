2852890628e76cfdb44a4b01bb093529df4b1158
Drasi96 <zahorak.andras@gmail.com>
2022-03-01 15:15:47 +0000

    end of lab 2 maybe

--- server.js
+++ server.js
@@ -23,6 +23,17 @@ adminRouter.get('/users', function(req, res) {
 adminRouter.get('/posts', function(req, res) {
  res.send('I show all the posts!'); });
 // apply the routes to our application
+ // route middleware to validate :name
+ adminRouter.param('name', function(req, res, next, name) {
+    // do validation on name here
+    // log something so we know its working
+    console.log('doing name validations on ' + name);
+    // once validation is done save the new item in the req
+    req.name = name;
+    // go to the next thing
+    next();
+    });
+   
  // route with parameters (http://localhost:PORT/admin/users/:name)
  adminRouter.get('/users/:name', function(req, res) {
     res.send('hello ' + req.params.name + '!'); }); 
@@ -35,6 +46,15 @@ adminRouter.use(function(req, res, next) {
     next(); });
    ///////////////////////////////////////////////////////////////////
 app.use('/admin', adminRouter);
+app.route('/login')
+// show the form (GET http://localhost:PORT/login)
+.get(function(req, res) {
+res.send('this is the login form');
+})
+// process the form (POST http://localhost:PORT/login)
+.post(function(req, res) { console.log('processing');
+res.send('processing the login form!');
+});
 
 
 });
