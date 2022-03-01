455ef38e170f61b562d817df71436947d5803cb7
Drasi96 <zahorak.andras@gmail.com>
2022-03-01 14:27:38 +0000

    lal

--- server.js
+++ server.js
@@ -7,10 +7,34 @@ const PORT = process.env.PORT || 8080;
 var port = PORT;
 // send our index.html file to the user for the home page
 app.get('/', function(req, res) {
+    ///////////////////////////////////////////////////////////////////
+// create routes for the admin section
+//get an instance of the router
+var adminRouter = express.Router();
+// admin main page. the dashboard (http://localhost:PORT/admin)
+adminRouter.get('/', function(req, res) {
+ res.send('I am the dashboard!'); });
+// users page (http://localhost:PORT/admin/users)
+adminRouter.get('/users', function(req, res) {
+ res.send('I show all the users!'); });
+// posts page (http://localhost:PORT/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+ res.send('I show all the posts!'); });
+// apply the routes to our application
+app.use('https://sleepy-savannah-40413.herokuapp.com/admin', adminRouter);
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next(); });
+   ///////////////////////////////////////////////////////////////////
     
  res.sendFile(__dirname + '/index.html');
 });
 // start the server
 app.listen(PORT);
-console.log('Express Server running at http://127.0.0.1:'.PORT);
+console.log('Express Server running at http://127.0.0.1:'+PORT);
 ///////////////////////////////////////////////////////////////////
